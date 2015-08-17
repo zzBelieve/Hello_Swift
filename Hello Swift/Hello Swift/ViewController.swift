5d71dc799ef5c6c1e14cf704b35fb7d37edf0e70
xiajun.wang <1043468081@qq.com>
2015-08-17 13:51:40 +0800

    First commit
    First commit 2015.8.17

---
+++ Hello Swift/Hello Swift/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Hello Swift
+//
+//  Created by ZZBelieve on 15/8/17.
+//  Copyright (c) 2015年 galaxy-link. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        println("Hello Swift")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
